dcf79aa97250d990c25179c45116caeb28edb1d8
andrewdyer <andrewdyer@outlook.com>
2024-12-12 22:53:30 +0000

    feat: :sparkles: configure path aliases

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-import { App } from './app/core';
+import { App } from '@/core';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,13 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src/app'),
+      '@shared': resolve(__dirname, 'src/shared'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,

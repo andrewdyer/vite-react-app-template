a9b19565ef4a5b9d86dacafc838ca26083edaa65
andrewdyer <andrewdyer@outlook.com>
2024-12-17 14:26:42 +0000

    feat: :sparkles: integrate Redux for HomePage component

--- src/modules/demo/pages/HomePage/HomePage.test.tsx
+++ src/modules/demo/pages/HomePage/HomePage.test.tsx
@@ -1,9 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
+import store from '@/store';
+
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
   test('should render the HomePage component without error', () => {
-    render(<HomePage />);
+    render(
+      <Provider store={store}>
+        <HomePage />
+      </Provider>
+    );
 
     expect(screen.getByAltText('Vite logo')).toBeInTheDocument();
     expect(screen.getByAltText('React logo')).toBeInTheDocument();

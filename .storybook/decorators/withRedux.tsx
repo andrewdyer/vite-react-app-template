9b5dec52af9d116c0002701e09d35437f2be8cb2
Andrew Dyer <andrewdyer@outlook.com>
2025-01-04 13:27:24 +0000

    fix: :bug: correct import statement
    Corrected import statement for Redux store in Storybook decorator

--- .storybook/decorators/withRedux.tsx
+++ .storybook/decorators/withRedux.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import { store } from '../../src/app/store';
+import store from '../../src/app/store';
 
 const withRedux = (Story) => (
   <Provider store={store}>

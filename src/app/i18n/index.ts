46f62738f038cc5ecf71a007e50dbc57fe04bbd0
Andrew Dyer <andrewdyer@outlook.com>
2025-01-04 16:38:24 +0000

    feat: :sparkles: add i18next-http-backend for dynamic translation loading

--- src/app/i18n/index.ts
+++ src/app/i18n/index.ts
@@ -1,17 +1,18 @@
 import i18n from 'i18next';
+import I18NextHttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {},
+i18n
+  .use(I18NextHttpBackend)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
     },
-  },
-  lng: 'en',
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-});
+    backend: {
+      loadPath: '/assets/locales/{{ns}}/{{lng}}.json',
+    },
+  });
 
 export default i18n;

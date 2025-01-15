988373f8e4828eb5bd8b0a16f928682a6b283026
Andrew Dyer <andrewdyer@outlook.com>
2025-01-15 10:17:21 +0000

    refactor: :hammer: remove user interaction story from Home component #3

--- src/modules/demo/pages/Home/Home.stories.ts
+++ src/modules/demo/pages/Home/Home.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { expect, userEvent, within } from '@storybook/test';
 
 import Home from './Home';
 
@@ -16,18 +15,3 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
-
-export const WithUserInteraction: Story = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const someButton = canvas.getByRole('button', { name: /Some Action/i });
-    await expect(someButton).toBeInTheDocument();
-    await userEvent.click(someButton);
-    await expect(someButton).not.toBeInTheDocument();
-
-    const anotherElement = canvas.getByRole('button', {
-      name: /Another Action/i,
-    });
-    await expect(anotherElement).toBeInTheDocument();
-  },
-};

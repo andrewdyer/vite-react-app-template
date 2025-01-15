921e507a62ef2268b6d8fa7dc5aefc7ca5c983c5
Andrew Dyer <andrewdyer@outlook.com>
2025-01-15 10:33:47 +0000

    refactor: :hammer: enhance Providers component test

--- src/app/providers/Providers.test.tsx
+++ src/app/providers/Providers.test.tsx
@@ -1,17 +1,17 @@
 import { render, screen } from '@testing-library/react';
 
-import Providers from './Providers';
+import Providers, { type ProvidersProps } from './Providers';
 
-const renderComponent = () => {
-  render(
-    <Providers>
-      <div data-testid="mock-component">Test Child</div>
-    </Providers>
-  );
+const defaultProps: ProvidersProps = {
+  children: <div data-testid="mock-component">Test Child</div>,
+};
+
+const renderComponent = (props: ProvidersProps = defaultProps) => {
+  return render(<Providers {...defaultProps} {...props} />);
 };
 
 describe('Providers', () => {
-  test('should render children correctly', () => {
+  test('should render the child component correctly', () => {
     renderComponent();
 
     const mockComponent = screen.getByTestId('mock-component');

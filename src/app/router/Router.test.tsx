d7c5a5c03af672f906fd23cd71da71dcb6aff30f
Andrew Dyer <andrewdyer@outlook.com>
2025-01-15 10:48:25 +0000

    test: :rotating_light: refactor Router component test for improved clarity and reusability

--- src/app/router/Router.test.tsx
+++ src/app/router/Router.test.tsx
@@ -1,24 +1,31 @@
 import { render, screen } from '@testing-library/react';
+
 import { MemoryRouter } from 'react-router-dom';
 
 import Router from './Router';
 
 vi.mock('@demo/pages', () => {
   const actual = vi.importActual('@demo/pages');
+
   return {
     ...actual,
     Home: () => <div data-testid="mock-home-page" />,
   };
 });
 
-describe('Router', () => {
-  test('should render Home for the root path', () => {
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <Router />
-      </MemoryRouter>
-    );
+const renderComponent = (initialEntries: string[] = ['/']) => {
+  return render(
+    <MemoryRouter initialEntries={initialEntries}>
+      <Router />
+    </MemoryRouter>
+  );
+};
 
-    expect(screen.getByTestId('mock-home-page')).toBeInTheDocument();
+describe('Router', () => {
+  test('should render the Home component when navigating to the root path', () => {
+    renderComponent();
+
+    const mockHomePage = screen.getByTestId('mock-home-page');
+    expect(mockHomePage).toBeInTheDocument();
   });
 });
